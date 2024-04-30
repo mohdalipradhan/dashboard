0f76fab702922eb4eb3eaaa0e95edae3b783e607
MOhammad Ali <mdsali914@gmail.com>
2024-05-01 04:13:55 +0530

    updated

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,5 @@
 export * from "./admin";
 
-export { default as BackToTop } from "./BackToTop";
-export { default as Footer } from "./Footer";
+export { default as BackToTop } from "../BackToTop";
+export { default as Footer } from "../Footer";
 export { default as Navbar } from "./Navbar";
